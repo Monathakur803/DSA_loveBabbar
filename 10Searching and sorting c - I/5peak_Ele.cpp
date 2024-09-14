28d45d15c10ece891e9d57e38e5a195726fe22be
Mona <monakumaridbg892@gmail.com>
2024-09-14 14:26:41 +0530

    Peak element in mountain arry done

--- 10Searching and sorting c - I/5peak_Ele.cpp
+++ 10Searching and sorting c - I/5peak_Ele.cpp
@@ -1,3 +1,28 @@
-// Find peak element in a mountain array
-// [0,7,8,10,5,3,2] this is mountain array  inc from start thrn peak element and then dec
- 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findPeakInex(vector<int> arr) {
+    int start = 0;
+    int end = arr.size() - 1;
+    int mid = start + (end - start) / 2;
+
+    while (start < end) {
+        if (arr[mid] < arr[mid + 1]) {
+            // if true then do right search
+            start = mid + 1;
+        } else {
+            end = mid;
+        }
+        // update mid value
+        mid = start + (end - start) / 2;
+    }
+    return start;
+}
+
+int main() {
+    vector<int> arr = {0, 2, 3, 4, 5, 3, 1};
+    int peakIndex = findPeakInex(arr);
+    cout << "The peak index is: " << peakIndex << endl;
+    return 0;
+}
